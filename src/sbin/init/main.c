6fe4bad31ad086ff7fadf94b6b78f6fc8419afce
chardida <74832292+chardida@users.noreply.github.com>
2025-07-28 21:28:46 +0100

    Update main.c

--- src/sbin/init/main.c
+++ src/sbin/init/main.c
@@ -1,15 +1,10 @@
 #include <unistd.h>
 #include <sys/mount.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
-
-int fd = open("/dev/console", O_RDWR);
-if (fd >= 0) {
-    dup2(fd, STDIN_FILENO);
-    dup2(fd, STDOUT_FILENO);
-    dup2(fd, STDERR_FILENO);
-}
 
 char* default_env[] = {
     "PATH=/bin:/usr/bin",
@@ -20,12 +15,24 @@ char* default_env[] = {
 };
 
 int main() {
+    // Mount essential virtual filesystems
     mount("devtmpfs", "/dev", "devtmpfs", 0, NULL);
     mknod("/dev/console", S_IFCHR | 0600, makedev(5, 1));
     mount("proc", "/proc", "proc", 0, NULL);
     mount("sysfs", "/sys", "sysfs", 0, NULL);
+
+    // Bind stdio to /dev/console
+    int fd = open("/dev/console", O_RDWR);
+    if (fd >= 0) {
+        dup2(fd, STDIN_FILENO);
+        dup2(fd, STDOUT_FILENO);
+        dup2(fd, STDERR_FILENO);
+    }
+
     puts("Launching shell...\n");
+
+    // Launch shell
     execle("/bin/sh", "sh", NULL, default_env);
-    perror("execl");
+    perror("execle");
     return 1;
 }
