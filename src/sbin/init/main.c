3c4fc88fa62559a6029a7c5022d658510c9ff0d2
chardida <74832292+chardida@users.noreply.github.com>
2025-07-28 21:28:16 +0100

    Update main.c

--- src/sbin/init/main.c
+++ src/sbin/init/main.c
@@ -2,6 +2,14 @@
 #include <sys/mount.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+
+int fd = open("/dev/console", O_RDWR);
+if (fd >= 0) {
+    dup2(fd, STDIN_FILENO);
+    dup2(fd, STDOUT_FILENO);
+    dup2(fd, STDERR_FILENO);
+}
 
 char* default_env[] = {
     "PATH=/bin:/usr/bin",
@@ -12,6 +20,8 @@ char* default_env[] = {
 };
 
 int main() {
+    mount("devtmpfs", "/dev", "devtmpfs", 0, NULL);
+    mknod("/dev/console", S_IFCHR | 0600, makedev(5, 1));
     mount("proc", "/proc", "proc", 0, NULL);
     mount("sysfs", "/sys", "sysfs", 0, NULL);
     puts("Launching shell...\n");
